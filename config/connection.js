58bac395f36f11147ccdc25d58657e66ec4e5042
Marly Valcin <marlyvalcin@gmail.com>
2024-07-21 16:26:29 -0400

    added comments to connection.js and updated server.js to run the server after the db is open

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+//Connects to MongoDB
 mongoose.connect(process.env.MONGOURI);
 
+//Exports Mongoose connection
 module.exports = mongoose.connection;
--- server.js
+++ server.js
@@ -7,3 +7,10 @@ const app = express();
 //Express Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//After the database is open then the server will start running 
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+    });
+});
