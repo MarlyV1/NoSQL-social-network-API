2db607303bba8daa3ba55e3b6d7faa730348d009
Marly Valcin <marlyvalcin@gmail.com>
2024-07-21 21:02:22 -0400

    created a virtual called friendCount

--- models/user.js
+++ models/user.js
@@ -27,5 +27,17 @@ const userSchema = new Schema(
             type: Schema.Types.ObjectId,
             ref: 'User'
         }]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false,
     }
-)
+);
+
+userSchema
+.virtual('friendCount')
+.get(function () {
+    return this.friends.length;
+});
