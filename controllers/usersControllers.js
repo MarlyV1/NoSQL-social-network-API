06837a10f4c69842c06306f9750deb7de3922414
Marly Valcin <marlyvalcin@gmail.com>
2024-07-31 19:25:20 -0400

    added createUser to the user controllers

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -20,4 +20,14 @@ module.exports = {
             console.error(error.message);
         }   
     },
+
+    async createUser(req, res) {
+        try {
+            const newUser = await User.create(req.body);
+            res.status(200).json(newUser);  
+        } catch (error) {
+            res.status(400).json(error);
+            console.error(error.message);
+        }  
+    }
 }
