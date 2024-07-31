47217bdade5f22a4c058ec178df9080140cc2118
Marly Valcin <marlyvalcin@gmail.com>
2024-07-31 19:22:21 -0400

    exported the models from the index file and created getOneUser in userControllers

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,12 +1,23 @@
-const { User } = require('../models/user');
+const { User } = require('../models');
 
 module.exports = {
     async getUsers(req, res) {
         try {
             const allUsers = await User.find();
-            res.json(allUsers);
+            res.status(200).json(allUsers);
         } catch (error) {
+            res.status(400).json(error);
             console.error(error.message);
         }
-    }
+    },
+
+    async getOneUser(req, res) {
+        try {
+            const singleUser = await User.findOne({ id: req.param.id});
+            res.json(singleUser);
+        } catch (error) {
+            res.status(400).json(message);
+            console.error(error.message);
+        }   
+    },
 }
