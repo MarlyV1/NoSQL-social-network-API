6289c8abd27acadbc789e3fc3c1f3babef45e016
Marly Valcin <marlyvalcin@gmail.com>
2024-07-31 18:43:59 -0400

    updated routes to create the api route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('./config/connection');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,8 @@ const app = express();
 //Express Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(routes);
 
 //After the database is open then the server will start running 
 db.once('open', () => {
