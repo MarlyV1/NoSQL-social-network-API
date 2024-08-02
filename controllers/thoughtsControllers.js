4a4d5dd0adca7e748643a8c66adb2a26d5c73429
Marly Valcin <marlyvalcin@gmail.com>
2024-08-02 13:10:29 -0400

    created the methods to get a single thought and to create a thought

--- controllers/thoughtsControllers.js
+++ controllers/thoughtsControllers.js
@@ -10,4 +10,31 @@ module.exports = {
             console.error(error.message);
         }
     },
+    async getSingleThought(req, res) {
+        try {
+            const singleThought = await Thought.findById(req.params.id);
+            if(!singleThought) {
+                return res.status(404).json('No thought found with that ID');
+            }
+            res.json(singleThought);
+        } catch (error) {
+            res.status(400).json(error);
+            console.error(error.message);
+        }
+    },
+    async createThought(req, res) {
+        try {
+            const newThought = await Thought.create(req.body);
+            const user = await User.findByIdAndUpdate(req.body.userId, {$push: {thoughts: newThought._id}}, {new: true});
+            if(!user) {
+                return res.status(404).json('Thought created, but no user found with that ID');
+            }
+            res.json(newThought);
+        } catch (error) {
+            res.status(400).json(error);
+            console.error(error.message);
+        }
+        },
+
+
 };
