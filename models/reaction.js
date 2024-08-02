5eab0dd93cc7f9ccc1e1abaf40df60cfdf4eb804
Marly Valcin <marlyvalcin@gmail.com>
2024-08-02 14:51:06 -0400

    created the reaction schema

--- models/reaction.js
+++ models/reaction.js
@@ -1,2 +1,38 @@
 const { Schema, Types } = require('mongoose');
 const dayJS = require('dayjs');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => dayJS(date).format('MM/DD/YYYY'),
+        },
+        username: {
+            type: String,
+            required: true
+        },
+    },
+    {
+        timestamps: true,
+        toJSON: {
+            getters: true,
+        },
+        _id: false,
+        id: false
+    }
+);
+
+
+
+
+module.exports = reactionSchema;
