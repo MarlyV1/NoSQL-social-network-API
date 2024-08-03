f7be9026bd3cc515ad467622b43dbbebf9a49f81
Marly Valcin <marlyvalcin@gmail.com>
2024-08-03 19:51:48 -0400

    updated the users routes

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const { getUsers, getOneUser, createUser } = require('../../controllers/userControllers');
+const { getUsers, getOneUser, createUser, updateUser, deleteUser, addFriend, removeFriend } = require('../../controllers/usersControllers');
 
 router.route('/').get(getUsers).post(createUser);
 
-router.route('/:id').get(getOneUser);
+router.route('/:id').get(getOneUser).put(updateUser).delete(deleteUser);
+
+router.route('/:id/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
